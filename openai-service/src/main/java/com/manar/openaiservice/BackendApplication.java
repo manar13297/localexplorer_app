789778189deb67f060f2b9dc81c216b9da1d9569
manar13297 <manarbadre13@gamil.com>
2024-01-30 18:22:11 +0100

    env var

--- openai-service/src/main/java/com/manar/openaiservice/BackendApplication.java
+++ openai-service/src/main/java/com/manar/openaiservice/BackendApplication.java
@@ -2,12 +2,12 @@ package com.manar.openaiservice;
 
 import com.manar.openaiservice.entities.Explorer;
 import com.manar.openaiservice.repositories.ExplorerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 
 import java.util.stream.Stream;
 
