802d09aee4d358e9dc333297823eb2028ce5d160
manar13297 <manarbadre13@gamil.com>
2024-01-30 18:30:49 +0100

    c

--- openai-service/src/main/java/com/manar/openaiservice/repositories/ExplorerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.manar.openaiservice.repositories;
-import com.manar.openaiservice.entities.Explorer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface ExplorerRepository extends JpaRepository<Explorer,Long> {
-    Optional<Explorer> findByUsername(String username);
-
-}
--- openai-service/src/main/java/com/manar/openaiservice/repositories/GeolocationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.manar.openaiservice.repositories;
-
-import com.manar.openaiservice.entities.Geolocation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GeolocationRepository extends JpaRepository<Geolocation,Long> {
-}
